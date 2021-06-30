b113f14c88eed1decb41ab59a35a7b7f08bff36f
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-06-30 23:58:54 +0900

    Create train.py

---
+++ 16. Word EMbeddings/embeddings/train.py
@@ -0,0 +1,56 @@
+import numpy as np
+from tqdm import tqdm
+import logging 
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from .dataset import W2VCorpus, W2VDataset
+from .model import W2VModel
+
+logging.basicConfig()
+logging.root.setLevel(logging.NOTSET)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+writer = SummaryWriter()
+
+corp = W2VCorpus("text8")
+pairs = corp.make_positive_pairs()
+train_data = W2VDataset(pairs)
+train_dataloader = DataLoader(train_data, batch_size=1024)
+loaders = {"train": train_dataloader}
+
+evice = torch.device('cuda' if torch.cuda.is_available else 'cpu')
+logger.info(f"Model will be trained on {device}")
+
+model = W2VModel(voc_size=len(corp.vocabulary), emb_size=300)
+model.to(device)
+optimzer = torch.optim.Adam(model.parameters(), lr=1e-3)
+criterion = nn.CrossEntropyLoss()
+num_epoch = 1
+
+for epoch in range(num_epoch):
+    running_loss = 0
+    size = 0
+    for batch_idx, data in enumerate(loaders['train']):
+        input = data['word'].to(device)
+        target = data['context'].to(device)
+        
+        optimzer.zero_grad()
+        output = model.forward(input)
+        loss = criterion(output, target)
+        
+        running_loss += loss.item()
+        size += len(data)
+
+        loss.backward()
+        optimzer.step()
+        
+    avg_loss = running_loss / size
+    
+    writer.add_scalar("Loss/train", avg_loss, epoch)
+    logger.debug(f"Loss: {avg_loss / (epoch + 1)}")
+
+writer.flush()
+writer.close()
