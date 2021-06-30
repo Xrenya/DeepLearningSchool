b8ac6207303082df19e995688312a629bf3dd846
Rinat <51479797+Xrenya@users.noreply.github.com>
2021-06-30 23:45:58 +0900

    Create model.py

---
+++ 16. Word EMbeddings/embeddings/model.py
@@ -0,0 +1,24 @@
+import logging 
+import torch.nn as nn
+from torch import Tensor
+
+class W2VModel(nn.Module) -> None:
+    def __init__(self, voc_size, emb_size):
+        super(W2VModel, self).__init__()
+        self.encoder = nn.Embedding(voc_size, emb_size)
+        self.decoder = nn.Linear(emb_size, voc_size, bias=False)
+        self.voc_size = voc_size
+        self.emb_dim = emb_size
+        self.initialize_weights()
+
+    def _forward_impl(self, x: Tensor) -> Tensor:
+        out = self.encoder(x)
+        return self.decoder(out)
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self._forward_impl(x)
+
+    def initialize_weights(self):
+        init_range = 0.5 / self.emb_dim
+        self.encoder.weight.data.uniform_(-init_range, init_range)
+        self.decoder.weight.data.uniform_(0, 0)
