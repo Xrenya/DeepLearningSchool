e695005c233381e8c373fcd268234c5270a58479
Rinat <sv-rinat@mail.ru>
2021-07-01 14:41:20 +0900

    train.py

--- 16. Word EMbeddings/embeddings/train.py
+++ 16. Word EMbeddings/embeddings/train.py
@@ -20,59 +20,71 @@ logging.root.setLevel(logging.NOTSET)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-writer = SummaryWriter()
 
-file_path = './output'
-filename = "model_w2v.pt"
-if os.path.isdir(file_path) is False:
-    os.mkdir(file_path)
+def run(args):
+    writer = SummaryWriter()
+    filename = args.checkpoint_file
+    file_path = args.file_path
+    if os.path.isdir(file_path) is False:
+        os.mkdir(file_path)
 
-corp = W2VCorpus("text8")
-pairs = corp.make_positive_pairs()
-train_data = W2VDataset(pairs)
-train_dataloader = DataLoader(train_data, batch_size=1024)
-loaders = {"train": train_dataloader}
+    corp = W2VCorpus(args.dataset_path)
+    pairs = corp.make_positive_pairs()
+    train_data = W2VDataset(pairs)
+    train_dataloader = DataLoader(train_data, batch_size=args.batch_size)
+    loaders = {"train": train_dataloader}
 
-device = torch.device('cuda' if torch.cuda.is_available else 'cpu')
-logger.info(f"Model will be trained on {device}")
+    if torch.cuda.is_available and args.device_gpu:
+        device = 'cuda'
+    else:
+        device = 'cpu'
 
-model = W2VModel(voc_size=len(corp.vocabulary), emb_size=300)
-model.to(device)
-optimzer = torch.optim.Adam(model.parameters(), lr=1e-3)
-criterion = nn.CrossEntropyLoss()
-num_epoch = 1
+    logger.info(f"Model will be trained on {device}")
 
-for epoch in range(num_epoch):
-    running_loss = 0
-    size = 0
-    for batch_idx, data in enumerate(loaders['train']):
-        input = data['word'].to(device)
-        target = data['context'].to(device)
+    model = W2VModel(voc_size=len(corp.vocabulary), emb_size=300)
+    model.to(device)
+    optimzer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    criterion = nn.CrossEntropyLoss()
+
+    for epoch in range(args.epochs):
+        running_loss = 0
+        size = 0
+        for batch_idx, data in enumerate(loaders['train']):
+            input = data['word'].to(device)
+            target = data['context'].to(device)
+            
+            optimzer.zero_grad()
+            output = model.forward(input)
+            loss = criterion(output, target)
+            
+            running_loss += loss.item()
+            size += len(data)
+
+            loss.backward()
+            optimzer.step()
+            
+        avg_loss = running_loss / size
         
-        optimzer.zero_grad()
-        output = model.forward(input)
-        loss = criterion(output, target)
-        
-        running_loss += loss.item()
-        size += len(data)
+        writer.add_scalar("Loss/train", avg_loss, epoch)
+        logger.debug(f"Loss: {avg_loss}")
 
-        loss.backward()
-        optimzer.step()
+    writer.flush()
+    writer.close()
         
-    avg_loss = running_loss / size
-    
-    writer.add_scalar("Loss/train", avg_loss, epoch)
-    logger.debug(f"Loss: {avg_loss}")
+    torch.save(
+        {
+            "model": model.state_dict(),
+            "optimzer": optimzer.state_dict(),
+            "epoch": num_epoch,
+            "loss": avg_loss
+        }, 
+        os.path.join(file_path, filename)
+    )
 
-writer.flush()
-writer.close()
-    
-torch.save(
-    {
-        "model": model.state_dict(),
-        "optimzer": optimzer.state_dict(),
-        "epoch": num_epoch,
-        "loss": avg_loss
-    }, 
-    os.path.join(file_path, filename)
-)
+@hydra.main(config_path="conf", config_name='config.yaml')
+def main(args):
+    print(args)
+    run(args)
+
+if __name__ == "__main__":
+    main()
